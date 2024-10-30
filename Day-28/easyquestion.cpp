fa0e556752b4b2c87ad52550ce8041cc5e07ef4b
Rhimkhan <rhim78975730@gmail.com>
2024-10-30 14:40:12 +0530

    Update

---
+++ Day-28/easyquestion.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int N1,N2,N3;
+printf("enter first number");
+scanf("%d",&N1);
+printf("enter second number");
+scanf("%d",&N2);
+N3=N1+N2;
+printf("sum of N1 and N2:%d",N3);
+}
